c9fadbaa71a0102201413dff763ae6b679b6fe66
Leonardo <leoccapra@gmail.com>
2024-02-11 00:46:12 -0300

    segundo Commit

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,14 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = ('Hora do Desafio')
+
+function verificarClique() {
+    console.log('O botão foi clicado')
+}
+
+function mostrarAlerta() {
+    alert('Eu amo JS')
+}
+
+function mostrarPrompt() {
+
+}
