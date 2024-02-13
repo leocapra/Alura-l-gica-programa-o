c136471790300afd00bc7040c07361ff1e73fafa
Leonardo <leoccapra@gmail.com>
2024-02-13 16:06:16 -0300

    02. Funções
    Tipo booleano; anatomia de uma função.

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,26 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p')
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//abaixo - com parametros, acima sem parametros
+
+let numeroAleatorio = gerarNumero()
+
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('o botão foi clicado')
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroAleatorio);
+}
+
+function gerarNumero() {
+    return parseInt(Math.random() * 10 + 1);
 }
