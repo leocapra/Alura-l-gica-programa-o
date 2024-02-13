25d68e3e6558a8d19d8bcb30dbc3731bd9cc3778
Leonardo <leoccapra@gmail.com>
2024-02-12 21:15:53 -0300

    terceiro commit

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -1,14 +1,24 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = ('Hora do Desafio')
+titulo.innerHTML = ('Hora do Desafio');
 
 function verificarClique() {
-    console.log('O botão foi clicado')
+    console.log('O botão foi clicado');
 }
 
 function mostrarAlerta() {
-    alert('Eu amo JS')
+    alert('Eu amo JS');
 }
 
 function mostrarPrompt() {
+    var cidade = prompt('Em qual cidade você mora?');
+    alert('Estive em '+ cidade + ' e lembrei de você');
+}
 
+function mostrarSoma() {
+    let primeiroNumero = prompt('Digite o primeiro número:')
+    let segundoNumero = prompt('Digite o segundo número:')
+    let num1 = Number(primeiroNumero);
+    let num2 = Number(segundoNumero);
+    let terceiroNumero = num1 + num2;
+    alert('A soma dos dois números são: ' + terceiroNumero)
 }
